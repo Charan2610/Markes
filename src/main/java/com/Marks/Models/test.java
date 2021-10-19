afd0d0faadf17771cbcd0468aba6734cdda8835f
Charan2610 <charanjit.singh@ultivic.com>
2021-10-19 13:02:37 +0530

    Add new Class

--- src/main/java/com/Marks/Models/test.java
+++ src/main/java/com/Marks/Models/test.java
@@ -1,4 +1,14 @@
 package com.Marks.Models;
 
 public class test {
+
+    private int id;
+    private String username;
+    private String father_Name;
+
+    public test(int id, String username, String father_Name) {
+        this.id = id;
+        this.username = username;
+        this.father_Name = father_Name;
+    }
 }
